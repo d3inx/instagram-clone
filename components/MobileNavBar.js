8ec28365f25d8de09eb4e84bf12ed862ba46d327
d3inx <mr.reza.rm8224@gmail.com>
2022-06-16 12:48:00 +0430

    main

--- components/MobileNavBar.js
+++ components/MobileNavBar.js
@@ -15,7 +15,11 @@ const MobileNavBar = () => {
   const { data: session } = useSession();
   const [open, setOpen] = useRecoilState(modalState);
   return (
-    <div className="fixed flex md:hidden items-center justify-between bottom-0 w-full p-4 bg-gray-400/60 rounded-t-3xl backdrop-blur">
+    <>
+      {
+        session ? 
+        (
+          <div className="fixed flex md:hidden items-center justify-between bottom-0 w-full p-4 bg-gray-400/60 rounded-t-3xl backdrop-blur">
       <HomeIcon onClick={() => Router.push({pathname : '/'})} className="h-7 w-7 cursor-pointer hover:scale-125 transform duration-200" />
       <GlobeAltIcon className="h-7 w-7 cursor-pointer hover:scale-125 transform duration-200" />
       <PlusCircleIcon
@@ -33,6 +37,10 @@ const MobileNavBar = () => {
           )}
       </div>
     </div>
+        )
+        : ('')
+      }
+    </>
   );
 };
 
